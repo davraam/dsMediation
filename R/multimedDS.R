53bab643e724829366734b4b38a1e10c25c45c43
davraam <avraam.demetris@gmail.com>
2021-08-22 16:50:45 +0300

    updates to v1.0.0

--- R/multimedDS.R
+++ R/multimedDS.R
@@ -9,17 +9,16 @@
 #' average direct effects under the homogeneous interaction assumption based on a
 #' varying-coefficient linear structural equation model. The function also performs
 #' sensitivity analysis with respect to the violation of the homogenous interaction
-#' assumption. The function can be used for both the single experiment design and
-#' the parallel design.
+#' assumption. The function can be used for the single experiment design.
 #' @param outcome a string character, the name of the outcome variable in 'data'.
 #' @param med.main a string character, the name of the mediator of interest. Under
 #' the parallel design this is the only mediator variable used in the estimation.
-#' @param med.alt vector of character strings indicating the names of the 
+#' @param med.alt.transmit vector of character strings indicating the names of the 
 #' post-treatment confounders, i.e., the alternative mediators affecting both the
-#' main mediator and outcome. Not needed for the parallel design.
+#' main mediator and outcome.
 #' @param treat a string character, the name of the treatment variable in 'data'.
 #' @param covariates.transmit vector of character strings representing the names of the
-#' pre-treatment covariates. Cannot be used for the parallel design.
+#' pre-treatment covariates.
 #' @param data a string character, the name of data frame containing all the 
 #' above variables.
 #' @param sims a number of bootstrap samples used for the calculation of 
@@ -30,19 +29,28 @@
 #' @author Demetris Avraam, for DataSHIELD Development Team
 #' @export
 #'
-multimedDS <- function(outcome, med.main, med.alt = NULL, treat, covariates.transmit = NULL, data, 
-                       sims = 1000, conf.level, seed){
+multimedDS <- function(outcome, med.main, med.alt.transmit, treat, covariates.transmit, 
+                       data, sims = 1000, conf.level, seed){
   
   data <- eval(parse(text=data), envir = parent.frame())
   
-  covariates <- unlist(strsplit(covariates.transmit, split=","))
+  if(!is.null(med.alt.transmit)){
+    med.alt <- unlist(strsplit(med.alt.transmit, split=","))
+  }else{
+    med.alt <- NULL
+  }  
+  if(!is.null(covariates.transmit)){
+    covariates <- unlist(strsplit(covariates.transmit, split=","))
+  }else{
+    covariates <- NULL
+  }  
   
   if(!is.null(seed)){
     set.seed(seed)
   }
 
-  m.med.out <- mediation::multimed(outcome = outcome, med.main = med.main, med.alt = med.alt, treat=treat, 
-                                   covariates = covariates, experiment = NULL, data = data, design = c("single", "parallel"),
+  m.med.out <- mediation::multimed(outcome = outcome, med.main = med.main, med.alt = med.alt, treat = treat, 
+                                   covariates = covariates, experiment = NULL, data = data, design = "single",
                                    sims = sims, R2.by = 0.01, conf.level = conf.level, weight = NULL)
   
   out <- summary(m.med.out)
